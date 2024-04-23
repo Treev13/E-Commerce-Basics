efc46e47271185722b34f442ac1b28d8d3fb7ad5
Treev13 <115178933+Treev13@users.noreply.github.com>
2024-04-23 20:19:33 +0200

    Create ProductList for the Home Page

---
+++ Task_2/frontend/src/Components/ProductList/ProductList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ProductList.css';
+import Item from '../Item/Item';
+
+const ProductList = ({products}) => {
+  
+  return (
+    <div className='product-list'>
+        <h1>Available Products:</h1>
+        <hr />
+        <div className="product-card">
+            {products.map((item,i)=>{
+                return <Item key={i} 
+                                id={item.id}
+                                image={item.image}
+                                name={item.name}
+                                price={item.price}/>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default ProductList;
