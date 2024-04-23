d3374e5f8b018c1ae0d7c46c9d42205ab907d22d
Treev13 <115178933+Treev13@users.noreply.github.com>
2024-04-23 20:16:39 +0200

    Create Item Component

---
+++ Task_2/frontend/src/Components/Item/Item.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Item.css';
+
+const Item = (props) => {
+  return (
+    <div className='item'>
+      <img src={`/images/${props.image}`} alt='' />
+      <p>{props.name}</p>
+      <div className="item-price">{props.price} Ft</div>
+      <button>Details</button>
+    </div>
+  )
+}
+
+export default Item;
