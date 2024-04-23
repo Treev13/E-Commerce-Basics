d61097591cd006bae344c44045a96985064a58f9
Treev13 <115178933+Treev13@users.noreply.github.com>
2024-04-23 19:50:55 +0200

    Add routes for products

---
+++ Task_2/backend/routes/Products.js
@@ -0,0 +1,41 @@
+import { getProducts, getProduct, createProduct } from '../src/database.js';
+import jwt from 'jsonwebtoken';
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const products = await getProducts();
+    res.status(200).send(products);
+});
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const product = await getProduct(id);
+    res.status(200).send(product);
+});
+
+router.post('/', authenticateToken, async (req, res) => {
+    console.log(req.body);
+    const { name, price, image, checked } = req.body;
+    let active = 0;
+    if (checked === 'on') {
+        active = 1;
+    }
+
+    const product = await createProduct(name, price, image, active);
+    res.status(201).send(product);
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+export default router;
