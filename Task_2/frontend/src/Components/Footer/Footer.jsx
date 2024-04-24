c666f69a2ac51fa9e3dbd8516921316d35abdb86
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2024-04-24 07:58:55 +0200

    Styling the Footer

--- Task_2/frontend/src/Components/Footer/Footer.jsx
+++ Task_2/frontend/src/Components/Footer/Footer.jsx
@@ -2,31 +2,29 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <div className="footer-logo">
-                <img src='/images/Logo.jpg' alt='Logo' />
-                <p>SZOVEG</p>
-            </div>
-            <ul className="footer-links">
-                <li>Company</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-            <div className="footer-icon">
-                <div className="footer-icons-container">
-                    <img src='/images/facebook_logo.webp' alt="Facebook" />
-                </div>
-                <div className="footer-icons-container">
-                    <img src='/images/instagram_logo.webp' alt="Instagram" />
-                </div>
-            </div>
-            <div className="footer-copyright">
-                <hr />
-                <p>Copyright @ 2024 - All Rigth Reserved</p>
-            </div>
+  return (
+    <div className='footer'>
+      <hr />
+      <div className='footer-main'>
+        <ul className="footer-links">
+          <li>Company</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div className="footer-icon">
+          <div className="footer-icons-container">
+            <img src='/images/facebook_logo.webp' alt="Facebook" />
+          </div>
+          <div className="footer-icons-container">
+            <img src='/images/instagram_logo.webp' alt="Instagram" />
+          </div>
         </div>
-    )
+      </div>
+      <div className="footer-copyright">
+        <p>Copyright @ 2024 - All Rigth Reserved</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer;
