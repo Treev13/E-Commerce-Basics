0b8d9d073fa4e5aef06e2678c55d8a0afa4c25b6
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2024-04-24 08:25:12 +0200

    Add Task 1 Solution

---
+++ Task_1/script.js
@@ -0,0 +1,25 @@
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+
+function showMap() {
+    const address = document.getElementById("address").value;
+    const geocoder = new google.maps.Geocoder();
+    
+    geocoder.geocode({ address: address }, (results, status) => {
+        if (status === "OK") {
+            map.setCenter(results[0].geometry.location);
+            const marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
