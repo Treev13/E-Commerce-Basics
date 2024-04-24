bdd1f021c22422c5a98edd2c96e027e66b631930
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2024-04-24 07:26:56 +0200

    Create Navbar Component

--- Task_2/frontend/src/Components/Navbar/Navbar.jsx
+++ Task_2/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,1 +1,19 @@
+import React from 'react'
+import './Navbar.css'
 
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <p>Katona Csaba - (06)70-6389896</p>
+      </div>
+      <ul className="nav-menu">
+        <li><Link to='/index'>Home</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/stats'>Stats</Link></li>
+      </ul>
+    </div>
+  )
+}
